6f7e7e65584d9552447d820550a72e180f0de6ec
Preston <prestonosoro56@gmail.com>
2022-11-23 02:22:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31,
+    num2  = 2,
+    num3 = 31,
+    num4 = 9;
+
+
+
+let  multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random()* 10  ) + 1;
+
+const mod = num3 % num4;
+
+const  max = Math.max(5,7,9,11,15,20);
